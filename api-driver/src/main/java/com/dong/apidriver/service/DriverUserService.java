fc95e031aebacd5b4f1a74a7865ce324ec8d6467
yadong <153827051@qq.com>
2024-01-13 15:56:29 +0800

    api-driver骨架搭建和远程调用service-driver-user修改司机用户信息

---
+++ api-driver/src/main/java/com/dong/apidriver/service/DriverUserService.java
@@ -0,0 +1,32 @@
+package com.dong.apidriver.service;
+
+import com.dong.apidriver.remote.ServiceDriverUserClient;
+import com.dong.internalcommon.request.DriverUserDTO;
+import com.dong.internalcommon.result.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DriverUserService {
+
+    @Autowired
+    private ServiceDriverUserClient serviceDriverUserClient;
+
+    /**
+     *  远程到用service-driver-user，新增司机用户
+     * @param driverUserDTO
+     * @return
+     */
+/*    public ResponseResult createDriverUser(DriverUserDTO driverUserDTO){
+        return serviceDriverUserClient.user(driverUserDTO);
+    }*/
+
+    /**
+     *  远程到用service-driver-user，新增司机用户
+     * @param driverUserDTO
+     * @return
+     */
+    public ResponseResult modifyDriverUser(DriverUserDTO driverUserDTO){
+        return serviceDriverUserClient.updateUser(driverUserDTO);
+    }
+}
