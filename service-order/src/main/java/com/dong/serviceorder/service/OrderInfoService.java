2bee56dc24afb9d16cf4df0dcc9cb741c506b4c0
yadong <153827051@qq.com>
2024-01-26 22:29:42 +0800

    intern 小技巧

--- service-order/src/main/java/com/dong/serviceorder/service/OrderInfoService.java
+++ service-order/src/main/java/com/dong/serviceorder/service/OrderInfoService.java
@@ -52,21 +52,22 @@ public class OrderInfoService {
 
     /**
      * 新增订单
+     *
      * @param orderDTO
      * @return
      */
-    public ResponseResult addOrderInfo(OrderDTO orderDTO){
+    public ResponseResult addOrderInfo(OrderDTO orderDTO) {
         OrderInfo orderInfo = new OrderInfo();
-        BeanUtils.copyProperties(orderDTO,orderInfo);
+        BeanUtils.copyProperties(orderDTO, orderInfo);
         // 城市编码+车辆类型是否开通
         // 放在这里判断原因：只有城市和车型开通了服务，才能获取到计价版本，以及用户请求
-        if(!isCityVehicleExists(orderDTO)){
+        if (!isCityVehicleExists(orderDTO)) {
             return ResponseResult.fail(CommonStatusEnum.CITY_SERVICE_NOT_EXISTS);
         }
         // 判断城市是否有可用司机
         String cityCode = orderDTO.getAddress();
         ResponseResult<Boolean> driverAvailable = serviceDriverUserClient.isDriverAvailable(cityCode);
-        if(!driverAvailable.getData()){
+        if (!driverAvailable.getData()) {
             return ResponseResult.fail(CommonStatusEnum.CITY_DRIVER_NOT_AVAILABLE);
         }
         // 获取最新的计价版本
@@ -75,24 +76,24 @@ public class OrderInfoService {
         orderInfo.setFareVersion(priceRuleResponse.getFareVersion());
         // 判断用户是否有订单还在继续
         int count = passengerOrderOnGoing(orderDTO.getPassengerId());
-        if(count != 0){
+        if (count != 0) {
             return ResponseResult.fail(CommonStatusEnum.ORDER_GOING_ON);
         }
         // 判断用户设备是否是黑名单
         String blackDeviceKey = RedisPrefixUtils.blackDeviceCodePrefix + orderDTO.getDeviceCode();
         Boolean aBoolean = stringRedisTemplate.hasKey(blackDeviceKey);
-        if(aBoolean){
+        if (aBoolean) {
             // blackDeviceKey 存在
             String s = stringRedisTemplate.opsForValue().get(blackDeviceKey);
-            Integer accessCount  = Integer.parseInt(s);
+            Integer accessCount = Integer.parseInt(s);
             //WYD TODO 2024-01-25: 访问次数应该为2，为了方便测试这里改为100
-            if(accessCount >= 100){
+            if (accessCount >= 100) {
                 return ResponseResult.fail(CommonStatusEnum.DEVICE_INVALID);
             }
             stringRedisTemplate.opsForValue().increment(blackDeviceKey);
-        }else{
+        } else {
             // blackDeviceKey 不存在，用户第一次下单
-            stringRedisTemplate.opsForValue().setIfAbsent(blackDeviceKey,OrderConstant.INIT_ORDER_COUNT,OrderConstant.BLACK_DEVICE_TIME, TimeUnit.HOURS);
+            stringRedisTemplate.opsForValue().setIfAbsent(blackDeviceKey, OrderConstant.INIT_ORDER_COUNT, OrderConstant.BLACK_DEVICE_TIME, TimeUnit.HOURS);
         }
         orderInfo.setOrderStatus(OrderConstant.ORDER_START);
         orderInfoMapper.insert(orderInfo);
@@ -105,12 +106,12 @@ public class OrderInfoService {
     }
 
 
-
     /**
-     *  周边终端搜索----派单
+     * 周边终端搜索----派单
+     *
      * @param orderInfo
      */
-    public synchronized void dispatchRealTimeOrder(OrderInfo orderInfo){
+    public void dispatchRealTimeOrder(OrderInfo orderInfo) {
         AroundSearchTerminalDTO aroundSearchTerminalDTO = new AroundSearchTerminalDTO();
         aroundSearchTerminalDTO.setCenter(orderInfo.getDepLatitude() + "," + orderInfo.getDepLongitude());
 
@@ -136,13 +137,17 @@ public class OrderInfoService {
                 ResponseResult<OrderDriverResponse> availableDriver = serviceDriverUserClient.getAvailableDriver(carId);
                 OrderDriverResponse orderDriverResponse = availableDriver.getData();
                 log.info("搜索到的终端对应的司机信息：" + orderDriverResponse);
-                if(availableDriver.getCode() != 1){
+                if (availableDriver.getCode() != 1) {
                     log.info("没有车辆的carId:" + carId + "对应的司机");
-                }else{
+                } else {
                     log.info("车辆Id：" + carId + "找到了正在出车的司机");
                     Long driverId = orderDriverResponse.getDriverId();
+
+
+                    //synchronized (driverId){  // 不能解决并发问题
+                    //synchronized ((driverId + "").intern()){  // 能解决并发问题
                     // 查询司机是否有正在进行的订单
-                    if (driverOrderOnGoing(driverId)  > 0) {
+                    if (driverOrderOnGoing(driverId) > 0) {
                         continue;
                     }
 
@@ -162,23 +167,25 @@ public class OrderInfoService {
                     orderInfo.setOrderStatus(OrderConstant.DRIVER_TAKE_ORDER);
 
                     flag = false;
+                    // 如果派单成功，则跳出循环
+                    orderInfoMapper.updateById(orderInfo);
+
+
                 }
             }
 
 
-            // 如果派单成功，则跳出循环
-            orderInfoMapper.updateById(orderInfo);
         }
     }
 
 
     //城市编码+车辆类型是否开通
-    private Boolean isCityVehicleExists(OrderDTO orderDTO){
+    private Boolean isCityVehicleExists(OrderDTO orderDTO) {
         String fareType = orderDTO.getFareType();
         int index = fareType.indexOf("$");
         PriceRuleDTO priceRuleDTO = new PriceRuleDTO();
-        priceRuleDTO.setCityCode(fareType.substring(0,index));
-        priceRuleDTO.setVehicleType(fareType.substring(index + 1,fareType.length()));
+        priceRuleDTO.setCityCode(fareType.substring(0, index));
+        priceRuleDTO.setVehicleType(fareType.substring(index + 1, fareType.length()));
 
         ResponseResult<Boolean> responseResult = servicePriceClient.ifExists(priceRuleDTO);
         return responseResult.getData();
@@ -186,13 +193,14 @@ public class OrderInfoService {
 
 
     /**
-     *  查找乘客进行中的订单
+     * 查找乘客进行中的订单
+     *
      * @param passengerId
      * @return
      */
-    public int passengerOrderOnGoing(Long passengerId){
+    public int passengerOrderOnGoing(Long passengerId) {
         LambdaQueryWrapper<OrderInfo> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(OrderInfo::getPassengerId,passengerId);
+        queryWrapper.eq(OrderInfo::getPassengerId, passengerId);
         queryWrapper.and(wrapper -> wrapper.eq(OrderInfo::getOrderStatus, OrderConstant.ORDER_START)
                 .or().eq(OrderInfo::getOrderStatus, OrderConstant.DRIVER_TAKE_ORDER)
                 .or().eq(OrderInfo::getOrderStatus, OrderConstant.ORDER_GOING_PASSENGER)
@@ -205,13 +213,14 @@ public class OrderInfoService {
     }
 
     /**
-     *  查找司机进行中的订单
+     * 查找司机进行中的订单
+     *
      * @param driverId
      * @return
      */
-    public int driverOrderOnGoing(Long driverId){
+    public int driverOrderOnGoing(Long driverId) {
         LambdaQueryWrapper<OrderInfo> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(OrderInfo::getDriverId,driverId);
+        queryWrapper.eq(OrderInfo::getDriverId, driverId);
         queryWrapper.and(wrapper -> wrapper.eq(OrderInfo::getOrderStatus, OrderConstant.DRIVER_TAKE_ORDER)
                 .or().eq(OrderInfo::getOrderStatus, OrderConstant.ORDER_GOING_PASSENGER)
                 .or().eq(OrderInfo::getOrderStatus, OrderConstant.DRIVER_ARRIVE_PICK)
