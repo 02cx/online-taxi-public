0f93d942ddddb744429f30eca28818d5b93979f3
yadong <153827051@qq.com>
2024-02-01 14:08:13 +0800

    redis代码优化

--- service-order/src/main/java/com/dong/serviceorder/config/RedisConfig.java
+++ service-order/src/main/java/com/dong/serviceorder/config/RedisConfig.java
@@ -3,16 +3,24 @@ package com.dong.serviceorder.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisAddress;
+    @Value("${spring.redis.port}")
+    private String redisPort;
+    @Value("${spring.redis.database}")
+    private Integer redisDatabase;
+
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.183.130:6379").setDatabase(1);
+        config.useSingleServer().setAddress("redis://"+ redisAddress +":" + redisPort).setDatabase(redisDatabase);
 
         return Redisson.create(config);
     }
